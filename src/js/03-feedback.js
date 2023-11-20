428938291ed1324bd17670851397480eef24e3bb
akurzyk1987 <134443000+akurzyk1987@users.noreply.github.com>
2023-11-20 20:04:27 +0100

    goit-js-hw-08 - Commit 4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
+
+form.addEventListener(
+  'input',
+  throttle(evt => {
+    localStorage.setItem(localStorageKey, evt.target.value);
+  }, 500)
+);
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
