ee915a3a5011dfd278d77ac28d8ece12a13c6855
akurzyk1987 <134443000+akurzyk1987@users.noreply.github.com>
2023-11-20 18:29:43 +0100

    goit-js-hw-08 - Commit 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('ul.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item gallery__link">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`;
+});
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
